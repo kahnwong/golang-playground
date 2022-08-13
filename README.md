e681e3a48278e46799aced70a1df818d90d8cdde
Karn Wong <karn.wong+github@mailbox.org>
2022-08-14 00:34:15 +0700

    try out some stuff

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 ## Todo
 
-- [ ] update setup script for vscode and required utils extensions
+- [ ] update setup script for vscode golang extension
+- [ ] add brew install golang
+- [ ] set go path
+
+## Setup
+
+```bash
+set GOPATH ~/go
+set PATH $PATH:$GOPATH/bin
+
+brew install golangci-lint
+go install golang.org/x/tools/cmd/goimports@latest
+```
+
+## Init
+
+```bash
+go mod init golang-playground
+```
--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	x := float32(3.0)
+	fmt.Println(x)
+
+	m := map[string]int{"one": 1, "two": 2}
+	fmt.Println(m)
+
+	// struct
+	type database struct {
+		hostname string
+		username string
+		password string
+	}
+	prodDB := database{
+		hostname: "localhost",
+		username: "root",
+		password: "password",
+	}
+	fmt.Println(prodDB)
+
+	// if-else
+	stockCount := 4
+	if stockCount > 5 {
+		fmt.Println("all good")
+	} else if stockCount < 5 {
+		fmt.Println("need to restock")
+	}
+
+	// for loop
+	for i := 0; i < stockCount; i++ {
+		fmt.Println(i)
+	}
 }
